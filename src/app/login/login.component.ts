4784e00cbcc35ee0589f4e716261c363ddaa0e42
xiaowei <liang.xiaowei@qq.com>
2017-04-06 20:31:47 +0800

    修改登录代码样式

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,27 +1,47 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject } from "@angular/core";
 
 @Component({
-  selector: 'app-login',
+  selector: "app-login",
   template: `
     <p>
       login Works!
     </p>
     <div>
-      username: <input #usernameRef type='text'>
-      password: <input #passwordRef type='password'>
-      <button (click)="onClick(usernameRef.value, passwordRef.value)">Login</button>
+      <form #formRef="ngForm" (ngSubmit)="onSubmit(formRef.value)">
+        <fieldset ngModelGroup="login">
+          username: <input type="text" name="usernameInput" required minlength="3" [(ngModel)]="username" #usernameRef="ngModel">
+          {{usernameRef.errors | json }}
+          <div *ngIf="usernameRef.errors?.required">this is required </div> 
+          <div *ngIf="usernameRef.errors?.minlength">should be at least 3 charactors</div>
+          <br>
+
+          password: <input type="password" name="passwordInput" required minlength="3" [(ngModel)]="password" #passwordRef="ngModel">
+          {{passwordRef.errors | json }}
+          <div *ngIf="passwordRef.errors?.required">this is required</div>
+          <div *ngIf="passwordRef.errors?.minlength">should be at least 3 charactors</div>
+          <br>
+
+          <button type="submit">Login</button>
+        </fieldset>
+      </form>
     </div>
   `,
-  styles: []
+  styles: [`
+    input.ng-invalid{border:3px solid red;}
+    input.ng-valid{border:3px solid green;}
+  `]
 })
 export class LoginComponent implements OnInit {
-
-  constructor(@Inject('auth') private authService) { }
+  // 定义成员变量
+  username = "";
+  password = "";
+  constructor(@Inject("auth") private authService) { }
 
   ngOnInit() {
   }
 
-  onClick(username, pwd) {
-    console.log("The auth result is:" + this.authService.loginWithCredentials(username, pwd));
+  onSubmit(formValue) {
+    console.log("The auth result is:" + 
+    this.authService.loginWithCredentials(formValue.login.usernameInput, formValue.login.passwordInput));
   }
 }
